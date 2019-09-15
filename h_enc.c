bd10bfb21a6d6f8200101c623d1f6ff9b3593956
Joseph Sigbeku <josephsgbku@gmail.com>
2019-09-15 17:26:33 +0100

    Implemented sift_up and insert operation

--- h_enc.c
+++ h_enc.c
@@ -2,6 +2,37 @@
 #include <stdlib.h>
 #include "h_enc.h"
 
+#define MAX_HEAP 1024
+
+int sift_up(int* data, int index) {
+	int parent = (index-1)/2;
+	while(data[parent]>data[index]) {
+		int temp = data[parent];
+		data[parent]=data[index];
+		data[index]=temp;
+		index = parent;
+		parent = (parent-1)/2;
+	}	
+}
+
+int insert(int* data, int size, int value) {
+	data[size]=value;
+	sift_up(data, size);
+	return size+1;
+}
+
+int find_max() {
+
+}
+
+int delete_min() {
+
+}
+
+int extract_min() {
+
+}
+
 int heapify(int *data, int i, int len) {
 	
 	printf("sd\n");
@@ -34,18 +65,32 @@ int heapify(int *data, int i, int len) {
 
 }
 
-int main() {
+void display_heap(int* data, int len) {
+	
+	for (int i=0; i<len; i++) {
+		printf("%i,",data[i]);
+	}
+	printf("\n");
 
-	int data[] = {2334,55,54,3,1,1,3,3,44,23,11,45,22,4,88,964,23,453,89,2327,5,22,4,67};
-	int len = sizeof(data)/sizeof(int);
+}
+
+void create_heap(int* data, int len) {
 	int mid = len/2;
 	printf("sds: %i\n",len);
 	for (int i = mid; i>=0; i--) {
 		heapify(data,i,len);
 	}
 
-	for (int i=0; i<len; i++) {
-		printf("%i,",data[i]);
-	}
-	printf("\n");
 }
+
+int main() {
+
+	int data[MAX_HEAP] = {2334,55,54,3,1,1,3,3,44,23,11,45,22,4,88,964,23,453,89,2327,5,22,4,67};
+	int heap_size = 24;
+	create_heap(data,heap_size);
+	display_heap(data,heap_size);
+	heap_size = insert(data,heap_size,0);
+	display_heap(data,heap_size);
+}
+
+
